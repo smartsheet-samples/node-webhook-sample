ba112244f448c9295b47aa339bcb7f7803f0696a
Steve Weil <steve.weil@smartsheet.com>
2018-02-01 11:18:48 -0800

    First light

---
+++ README.md
@@ -0,0 +1,39 @@
+# node-webhook
+This sample node application demonstrates how to create a Shartsheet webhook and handle callback events.
+
+Smartsheet webhook documentation is here [http://smartsheet-platform.github.io/api-docs/#webhooks](http://smartsheet-platform.github.io/api-docs/#webhooks)
+
+## Installation
+Make sure you have [Node.js](http://nodejs.org/) installed.
+
+For code clarity, this sample uses _async_ and _await_ so requires node.js version 7.6 or later.
+
+```sh
+git clone git@github.com:smartsheet-platform/node-webhook.git # or clone your own fork
+cd node-webhook
+npm install
+```
+
+## Determining the callback url
+Webhooks are HTTP callbacks from Smartsheet servers to your code. So your web application must be accesible from the
+public internet. An easy way to achieve this in a development environment is with the tool [ngrok](https://ngrok.com/).
+
+After installing, start ngrok with the command `ngrok http 3000`. In the resulting screen, note the https forwarding url
+which will look something like `https://fbc72a6b.ngrok.io`.
+Update the `config.json` file with this value.
+
+Leave ngrok running. If you restart ngrok, you will get a different callback url and will need to update config.json and
+restart this applicaion.
+
+## Configuration
+Create a test sheet to monitor. Every change to that sheet will result in a webhook callback. Determine the sheet id, 
+visible in the sheet properties dialog.
+
+Update the `config.json` file with:
+* Your Smartsheet [access token](http://smartsheet-platform.github.io/api-docs/#authentication-and-access-tokens)
+* The id of the sheet you wish to monitor
+
+## Running
+```sh 
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,178 @@
+let ssClient;           // Smartsheet JS client object
+
+// Dependent libraries
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const ssSdk = require("smartsheet");
+
+// This method receives the webhook callbacks from Smartsheet
+app.post("/", async (req, res) => {
+    try {
+        const body = req.body;
+
+        // Callback could be due to validation, status change, or actual sheet change events
+        if (body.challenge) {
+            console.log("Received verification callback");
+            // Verify we are listening by echoing challenge value
+            res.status(200)
+                .json({ smartsheetHookResponse: body.challenge });
+        } else if (body.events) {
+            console.log(`Received event callback with ${body.events.length} events`);
+            res.status(200);
+
+            // Note that the callback response must be recieved within a few seconds.
+            // If you are doing complex processing, you may need to queue up pending work.
+            await processEvents(body);
+        } else if (body.newWebHookStatus) {
+            console.log(`Received status callback, new status: ${body.newWebHookStatus}`);
+            res.status(200);
+        } else {
+            console.log(`Received unknown callback: ${body}`);
+            res.status(200);
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+    }
+});
+
+/*
+* Process callback events
+* This sample implementation only logs to the console.
+* Your implementation might make updates or send data to another system.
+* Beware of infinte loops if you make modifications to the same sheet
+*/
+async function processEvents(callbackData) {
+    if (callbackData.scope !== "sheet") {
+        return;
+    }
+
+    // This sample handles each event individually.
+    // Some changes (e.g. column rename) could impact a large number of cells.
+    // A complete implementation should consolidate related events and/or cache intermediate data
+    for (const event of callbackData.events) {
+        // This sample only considers cell changes
+        if (event.objectType === "cell") {
+            console.log(`Cell changed, row id: ${event.rowId}, column id ${event.columnId}`);
+
+            // Since event data is "thin", we need to read from the sheet to get updated values.
+            const options = {
+                id: callbackData.scopeObjectId,             // Get sheet id from callback
+                queryParameters: {
+                    rowIds: event.rowId.toString(),         // Just read one row
+                    columnIds: event.columnId.toString()    // Just read one column
+                }
+            };
+            const response = await ssClient.sheets.getSheet(options);
+            const row = response.rows[0];
+            const cell = row.cells[0];
+            const column = response.columns.find(c => c.id === cell.columnId);
+            console.log(`**** New cell value "${cell.displayValue}" in column "${column.title}", row number ${row.rowNumber}`);
+        }
+    }
+}
+
+// Check that we can access the sheet
+async function probeSheet(targetSheetId) {
+// Confirm that we can access the sheet
+    console.log(`Checking for sheet id: ${targetSheetId}`);
+    const getSheetOptions = {
+        id: targetSheetId,
+        queryParameters: { pageSize: 1 } // Only return first row to reduce payload
+    };
+    const sheetResponse = await ssClient.sheets.getSheet(getSheetOptions);
+    console.log(`Found sheet: "${sheetResponse.name}" at ${sheetResponse.permalink}`);
+}
+
+/*
+* A webhook only needs to be created once.
+* But hooks will be disabled if validation or callbacks fail.
+* This method looks for an existing matching hook to reuse, else creates a new one.
+*/
+async function initializeHook(targetSheetId, hookName, callbackUrl) {
+    try {
+        let webhook = null;
+
+        // Get *all* my hooks
+        const listHooksResponse = await ssClient.webhooks.listWebhooks({
+            includeAll: true
+        });
+        console.log(`Found ${listHooksResponse.totalCount} hooks owned by user`);
+
+        // Check for hooks on this sheet, for this app
+        for (const hook of listHooksResponse.data) {
+            if (hook.scopeObjectId === targetSheetId &&
+                hook.name === hookName
+            ) {
+                webhook = hook;
+                console.log(`Found matching hook with id: ${webhook.id}`);
+                break;
+            }
+        }
+
+        if (!webhook) {
+            // Can't use any existing hook - create a new one
+            const options = {
+                body: {
+                    name: hookName,
+                    callbackUrl,
+                    scope: "sheet",
+                    scopeObjectId: targetSheetId,
+                    events: ["*.*"],
+                    version: 1
+                }
+            };
+
+            const createResponse = await ssClient.webhooks.createWebhook(options);
+            webhook = createResponse.result;
+
+            console.log(`Created new hook: ${webhook.id}`);
+        }
+
+        // Make sure webhook is enabled and pointing to our current url
+        const options = {
+            webhookId: webhook.id,
+            callbackUrl: callbackUrl,
+            body: { enabled: true }
+        };
+
+        const updateResponse = await ssClient.webhooks.updateWebhook(options);
+        const updatedWebhook = updateResponse.result;
+        console.log(`Hook enabled: ${updatedWebhook.enabled}, status: ${updatedWebhook.status}`);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Initialize client SDK
+function initializeSmartsheetClient(token, logLevel) {
+    ssClient = ssSdk.createClient({
+        // If token is falsy, value will be read from SMARTSHEET_ACCESS_TOKEN environment variable
+        accessToken: token,
+        logLevel: logLevel
+    });
+}
+
+// main
+(async () => {
+    try {
+        // TODO: Edit config.json to set desired sheet id and API token
+        const config = require("./config.json");
+
+        initializeSmartsheetClient(config.accessToken, config.logLevel);
+
+        // Sanity check: make sure we can access the sheet
+        await probeSheet(config.sheetId);
+
+        app.listen(3000, () =>
+            console.log("Node-webhook sample app listening on port 3000!"));
+
+        await initializeHook(config.sheetId, config.webhookName, config.callbackUrl);
+    } catch (err) {
+        console.error(err);
+    }
+})();
